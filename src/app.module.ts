196df8dfbda9e3ce7a66302964fdf5f67577d066
AlexeyFL <alexeyfl563@gmail.com>
2021-06-29 18:39:10 +0300

    feat: add postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { join } from 'path';
+import {
+  POSTGRES_DB,
+  POSTGRESS_HOST,
+  POSTGRESS_PORT,
+  POSTGRES_PASSWORD,
+  POSTGRES_USER,
+} from './common/config';
 import { UserController } from './nest-controllers/user.controller';
 import { BoardController } from './nest-controllers/board.controller';
 import { TaskController } from './nest-controllers/task.controller';
@@ -7,7 +16,24 @@ import { BoardService } from './nest-services/board.service';
 import { TaskService } from './nest-services/task.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: POSTGRESS_HOST,
+      port: Number(POSTGRESS_PORT),
+      username: POSTGRES_USER,
+      password: POSTGRES_PASSWORD,
+      database: POSTGRES_DB,
+      synchronize: true,
+      // migrationsRun: true,
+      entities: [join(__dirname, 'entity/*{.ts,.js}')],
+      migrations: [join(__dirname, 'migrations/*{.ts,.js}')],
+      cli: {
+        entitiesDir: 'src/entity',
+        migrationsDir: 'src/migrations',
+      },
+    }),
+  ],
   controllers: [UserController, BoardController, TaskController],
   providers: [UserService, BoardService, TaskService],
 })
